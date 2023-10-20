6be366fcfbf27c1d163a9436762be3bcdf2b8f52
Patrycja <patrycjavmarkiewicz@gmail.com>
2023-10-20 11:16:48 +0200

    project set up

---
+++ src/main/java/com/mpm/beforeandafter/BeforeAndAfterApplication.java
@@ -0,0 +1,13 @@
+package com.mpm.beforeandafter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeforeAndAfterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeforeAndAfterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpm/beforeandafter/BeforeAndAfterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpm.beforeandafter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeforeAndAfterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

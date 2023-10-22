913ea48d48d2900738bc89283c57193c78d98d12
Marcin Kącki <kacki.marcin@gmail.com>
2023-10-22 18:53:39 +0200

    Refactor: Added CommandLineRunner for RoleService initialization
    - Updated the application to include a CommandLineRunner for RoleService
      initialization during startup.
    - Created a CommandLineRunner bean to execute the creation of roles using
      RoleService.
    - This change ensures that roles are created at the application's launch.
    
    No associated issues

--- src/main/java/com/mpm/beforeandafter/BeforeAndAfterApplication.java
+++ src/main/java/com/mpm/beforeandafter/BeforeAndAfterApplication.java
@@ -1,13 +1,21 @@
 package com.mpm.beforeandafter;
 
+import com.mpm.beforeandafter.role.service.RoleService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BeforeAndAfterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BeforeAndAfterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BeforeAndAfterApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner commandLineRunner(RoleService roleService) {
+        return runner -> roleService.createRoles();
+    }
 }
+

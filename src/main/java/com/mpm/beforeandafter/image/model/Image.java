7e948eeb14353a928e3e0d19d0a8922f9b70ef67
magdalenahuget <magdalena.huget@gmail.com>
2023-10-23 14:55:24 +0200

    Feat: Added OneToOne annotation to user field

--- src/main/java/com/mpm/beforeandafter/image/model/Image.java
+++ src/main/java/com/mpm/beforeandafter/image/model/Image.java
@@ -2,6 +2,7 @@ package com.mpm.beforeandafter.image.model;
 
 import com.mpm.beforeandafter.service_category.model.Category;
 import com.mpm.beforeandafter.status.type.StatusesType;
+import com.mpm.beforeandafter.user.model.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -21,31 +22,32 @@ public class Image {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="image_id")
+    @Column(name = "image_id")
     private Long id;
 
-    @Column(name="image")
+    @Column(name = "image")
     private String image;
 
     @ManyToOne
-    @JoinColumn(name="service_category_id", nullable = false)
+    @JoinColumn(name = "service_category_id", nullable = false)
     private Category category;
 
-    @Column(name="image_description")
+    @Column(name = "image_description")
     private String description;
 
-    @Column(name="user_id")
-    private Long userId;
+    @OneToOne
+    @JoinColumn(name = "user_id", unique = true, nullable = false)
+    private User user;
 
-    @Column(name="image_approved")
+    @Column(name = "image_approved")
     private boolean isApproved;
 
-    @Column(name="image_approved_by")
+    @Column(name = "image_approved_by")
     private String approvedBy;
 
-    @Column(name="image_approved_date")
+    @Column(name = "image_approved_date")
     private LocalDate approvedDate;
 
-    @Column(name="status")
+    @Column(name = "status")
     private StatusesType status;
 }

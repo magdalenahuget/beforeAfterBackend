4f1ca59c9278e87bf6e286d44d2ec0de79a7b94c
Patrycja <patrycjavmarkiewicz@gmail.com>
2023-10-23 10:57:03 +0200

    Feat: Create CategoryService interface for Category entity service operations
    Created a new interface named CategoryService in the 'com.mpm.beforeandafter.service_category.service' package.
    Defined methods for CRUD operations and retrieval of Category entities such as createCategory, getCategories, getCategoryById, updateCategory, and deleteCategory.
    Added a comment hinting at a future implementation of a Data Transfer Object (DTO) class to manage data flow between the client and the database.
    This interface lays the foundation for encapsulating business logic related to Category entities, ensuring a separation of concerns between the service layer and other parts of the application.
    The CategoryService interface will serve as a contract for any implementations, providing a clear structure for managing Category entity operations.

---
+++ src/main/java/com/mpm/beforeandafter/service_category/service/CategoryService.java
@@ -0,0 +1,23 @@
+package com.mpm.beforeandafter.service_category.service;
+
+import com.mpm.beforeandafter.service_category.model.Category;
+
+import java.util.Set;
+
+public interface CategoryService {
+
+    /*
+       in the future create a DTO class to handle the flow of data between the client and the
+       database.
+     */
+
+    Category createCategory(Category category);
+
+     Set<Category> getCategories(Long categoryId);
+
+    Category getCategoryById(Long categoryId);
+
+    Category updateCategory(Long categoryId, Category category);
+
+    void deleteCategory(Long categoryId);
+}

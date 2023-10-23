15948cffe8d8a49e6d43e8824c41a6457896e473
Patrycja <patrycjavmarkiewicz@gmail.com>
2023-10-23 23:55:02 +0200

    Refactor: Update CategoryController logging and response type -Changed log level from info to debug for method entry logs. -Updated getCategories() response type from Set to List for consistency. -Kept info level logging for significant events like category creation and deletion.

--- src/main/java/com/mpm/beforeandafter/category/controller/CategoryController.java
+++ src/main/java/com/mpm/beforeandafter/category/controller/CategoryController.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Set;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -25,22 +25,22 @@ public class CategoryController {
 
     @PostMapping
     public ResponseEntity<Category> createCategory(@RequestBody Category categoryDTO) {
-        log.info("Creating category: {}", categoryDTO);
+        log.debug("Creating category: {}", categoryDTO);
         Category savedCategory = categoryService.createCategory(categoryDTO);
         log.info("Category created: {}", savedCategory);
         return new ResponseEntity<>(savedCategory, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<Set<Category>> getCategories() {
-        log.info("Getting all categories:");
-        Set<Category> categoriesDTO = categoryService.getCategories();
+    public ResponseEntity<List<Category>> getCategories() {
+        log.debug("Getting all categories:");
+        List<Category> categoriesDTO = categoryService.getCategories();
         return ResponseEntity.ok(categoriesDTO);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<Category> getCategoryById(@PathVariable("id") Long categoryId) {
-        log.info("Getting category by id: {}", categoryId);
+        log.debug("Getting category by id: {}", categoryId);
         Category categoryDTO = categoryService.getCategoryById(categoryId);
         return ResponseEntity.ok(categoryDTO);
     }
@@ -48,7 +48,7 @@ public class CategoryController {
     @PatchMapping("{id}/name")
     public ResponseEntity<Category> updateCategoryName(@PathVariable("id") Long categoryId,
                                                        @RequestBody String categoryName) {
-        log.info("Updating category with id: {} with data: {}", categoryId, categoryName);
+        log.debug("Updating category with id: {} with data: {}", categoryId, categoryName);
         Category categoryDTO = categoryService.updateCategoryName(categoryId, categoryName);
         log.info("Category updated: {}", categoryDTO);
         return ResponseEntity.ok(categoryDTO);
@@ -56,7 +56,7 @@ public class CategoryController {
 
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteCategory(@PathVariable("id") Long categoryId) {
-        log.info("Deleting category with id: {}", categoryId);
+        log.debug("Deleting category with id: {}", categoryId);
         categoryService.deleteCategory(categoryId);
         log.info("Category with id: {} deleted successfully", categoryId);
         return ResponseEntity.ok("Category deleted successfully!");

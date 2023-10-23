e5a452cbec30b35639ca941679edf1596b59d07d
Patrycja <patrycjavmarkiewicz@gmail.com>
2023-10-23 10:46:01 +0200

    Feat: Introduce CategoryController for Category entity API operations
    Created a new class named CategoryController in the 'com.mpm.beforeandafter.service_category.controller' package.
    Annotated the class with @RestController and @RequestMapping to map it to the 'api/v1/categories' endpoint.
    Applied the @Slf4j annotation to enable logging capabilities within the CategoryController.
    This class is designed to serve as the controller for handling HTTP requests related to Category entities, thus providing the necessary endpoints for managing categories through the API.
    With the setup of CategoryController, the application now has a structured way to handle client requests for Category entity operations, paving the way for implementing specific API methods like GET, POST, PUT, and DELETE.

---
+++ src/main/java/com/mpm/beforeandafter/service_category/controller/CategoryController.java
@@ -0,0 +1,13 @@
+package com.mpm.beforeandafter.service_category.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("api/v1/categories")
+public class CategoryController {
+
+}

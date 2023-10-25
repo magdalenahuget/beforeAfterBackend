bbff0742486fdbf2260ea40bc1a8b1c1dc64b766
Marcin Kącki <kacki.marcin@gmail.com>
2023-10-25 07:19:15 +0200

    Refactor: Renamed and update Address entity class to ContactDetails
    - Renamed the Address entity class to ContactDetails for improved clarity.
    - Removed the @NotNull, @NotBlank, and @NonNull annotations from the ContactDetails class fields as they are not required for user construction.
    - Removed the @RequiredArgsConstructor annotation as it is no longer used.
    -Added the "phone_number" and "webpage" fields

--- src/main/java/com/mpm/beforeandafter/contactdetails/model/ContactDetails.java
+++ src/main/java/com/mpm/beforeandafter/contactdetails/model/ContactDetails.java
@@ -1,43 +1,43 @@
-package com.mpm.beforeandafter.address.model;
+package com.mpm.beforeandafter.contactdetails.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@RequiredArgsConstructor
 
 @Entity
-@Table(name = "addresses")
-public class Address {
+@Table(name = "contact_details")
+public class ContactDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "address_id")
+    @Column(name = "id")
     private Long id;
 
-    @Column(name="street_name")
-    @NotBlank(message = "Street name cannot be blank")
+    @Column(name = "street_name")
     @Size(max = 30, message = "Street name cannot exceed 30 characters")
-    @NonNull
     private String streetName;
 
-    @Column(name="street_num")
-    @NotBlank(message = "Street number cannot be blank")
+    @Column(name = "street_num")
     @Size(max = 10, message = "Street number cannot exceed 10 characters")
-    @NonNull
     private String streetNumber;
 
-    @Column(name="apart_num")
+    @Column(name = "apart_num")
     @Size(max = 10, message = "Apartment number cannot exceed 10 characters")
     private String apartNumber;
 
-    @Column(name="postcode")
-    @NotBlank(message = "Postcode cannot be blank")
+
     @Size(max = 10, message = "Postcode cannot exceed 10 characters")
-    @NonNull
     private String postcode;
+
+    @Column(name = "phone_number", length = 15)
+    private String phoneNumber;
+
+    @Column(name = "webpage", length = 150)
+    private String webpage;
 }

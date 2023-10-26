d428ff5c6482a07926ee9ade4a241de29563d8ae
Marcin Kącki <kacki.marcin@gmail.com>
2023-10-26 14:02:01 +0200

    Refactor: Moved "cityName" field to the Image entity
    - Relocated the "cityName" field from the User entity to the Image entity.
    - Removed the @Size annotation from the "cityName" field, as it's no longer relevant in the Image entity context.

--- src/main/java/com/mpm/beforeandafter/user/model/User.java
+++ src/main/java/com/mpm/beforeandafter/user/model/User.java
@@ -47,9 +47,7 @@ public class User {
     @JsonIgnore
     private Role role;
 
-    @Column(name = "city_name", length = 100)
-    @Size(min=2, max=100,message="User password must be between 2 and 100 characters long.")
-    private String cityName;
+
 
     @Column(name = "avatar", unique = true)
     private String avatar; //TODO:change type to proper type for images

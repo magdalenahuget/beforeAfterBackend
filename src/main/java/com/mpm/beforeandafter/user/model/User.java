3d4652898e93eedeeb8ee43c9148c7c92c2e7d1b
Marcin Kącki <kacki.marcin@gmail.com>
2023-10-26 20:47:57 +0200

    Update User entity class
    - Updated the User entity class in the com.mpm.beforeandafter.user.model package.
    - Replaced the data type of "contactId" and "approvedByUserId" fields from primitive int to Integer for flexibility.
    - Removed the "nullable = true" attribute for the "approvedDate" field, as it's not required.
    - This change reflects the refactoring and optimization of the User entity.

--- src/main/java/com/mpm/beforeandafter/user/model/User.java
+++ src/main/java/com/mpm/beforeandafter/user/model/User.java
@@ -25,20 +25,20 @@ public class User {
 
     @Column(name = "user_name", length = 50, unique = true)
     @NotBlank(message = "User name is mandatory.")
-    @Size(min=2, max=25,message="User name must be between 2 and 25 characters long.")
+    @Size(min = 2, max = 25, message = "User name must be between 2 and 25 characters long.")
     @NotNull
     private String userName;
 
     @Column(name = "user_email", length = 100, unique = true)
     @NotNull
     @NotBlank(message = "User email is mandatory.")
-    @Size(min=5, max=50,message="User email must be between 5 and 50 characters long.")
+    @Size(min = 5, max = 50, message = "User email must be between 5 and 50 characters long.")
     private String userEmail;
 
     @Column(name = "user_password", length = 200)//TODO security hash?
     @NotBlank(message = "User password is mandatory.")
     @NotNull
-    @Size(min=8, max=50,message="User password must be between 8 and 50 characters long.")
+    @Size(min = 8, max = 50, message = "User password must be between 8 and 50 characters long.")
     private String userPassword;
 
     @NotNull
@@ -49,19 +49,18 @@ public class User {
     private Role role;
 
     @Column(name = "city_name", length = 100)
-    @Size(min=2, max=100,message="User password must be between 2 and 100 characters long.")
+    @Size(min = 2, max = 100, message = "User password must be between 2 and 100 characters long.")
     private String cityName;
 
     @Column(name = "user_profile_image", unique = true)
     private String userProfileImage; //TODO:change type to proper type for images, and rename to "avatar"(?)
 
     @Column(name = "contact_id")//TODO relation with table addresses oneToOne
-    private int contactId;
+    private Integer contactId;
 
 
     @Column(name = "about_me", columnDefinition = "TEXT")
     @Size(min = 10, max = 500, message = "About me must be between 10 and 500 characters.")
-    @Lob
     private String aboutMe;
 
     @Enumerated(EnumType.STRING)
@@ -73,8 +72,8 @@ public class User {
     @NotNull(message = "Approved status is mandatory.")
     private boolean approved;
 
-    @Column(name = "approved_by_user_id", nullable = true)
-    private int approvedByUserId;
+    @Column(name = "approved_by_user_id")
+    private Integer approvedByUserId;
 
     @Column(name = "approved_date", nullable = true)
     private String approvedDate;

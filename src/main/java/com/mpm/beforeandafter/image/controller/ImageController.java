39c6d926696d1b76773ca11d0fd2ad3fb9b036a6
Marcin Kącki <kacki.marcin@gmail.com>
2023-10-26 22:39:02 +0200

    Modified the ImageController class in the com.mpm.beforeandafter.image.controller package
    - Adding a getImages method to handle GET requests, which returns a list of GetImagesResponseByStatusApproval objects.
    -Changing the endpoint path from @RequestMapping("/api/images") to @RequestMapping("/api/v1/images"),

--- src/main/java/com/mpm/beforeandafter/image/controller/ImageController.java
+++ src/main/java/com/mpm/beforeandafter/image/controller/ImageController.java
@@ -2,24 +2,33 @@ package com.mpm.beforeandafter.image.controller;
 
 import com.mpm.beforeandafter.image.dto.CreateImageRequest;
 import com.mpm.beforeandafter.image.dto.CreateImageResponse;
+import com.mpm.beforeandafter.image.dto.GetImagesRequestByStatusApproval;
+import com.mpm.beforeandafter.image.dto.GetImagesResponseByStatusApproval;
 import com.mpm.beforeandafter.image.model.Image;
 import com.mpm.beforeandafter.image.service.ImageService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static com.mpm.beforeandafter.image.dto.CreateImageResponse.map;
+import static com.mpm.beforeandafter.image.dto.GetImagesResponseByStatusApproval.mapByStatusApproval;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/images")
+@RequestMapping("/api/v1/images")
 public class ImageController {
     private final ImageService imageService;
 
     public ImageController(ImageService imageService) {
         this.imageService = imageService;
+    }
+    @GetMapping
+    public List<GetImagesResponseByStatusApproval> getImages(@RequestBody GetImagesRequestByStatusApproval request){
+        log.debug("Get all images by approval status: {}", request);
+        List<Image> images = imageService.getImagesByApprovalStatus(request);
+        log.info("Images by status approval: {}", images);
+        return mapByStatusApproval(images);
     }
 
     @PostMapping

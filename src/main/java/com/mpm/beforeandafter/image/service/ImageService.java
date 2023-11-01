bebb76e1a3bfc65d4222a7faa326ab9fe7d6a521
Marcin Kącki <kacki.marcin@gmail.com>
2023-11-01 08:11:59 +0100

    refactor: Updated ImageService Interface for Consistency
    Refactored the ImageService interface for consistency and clarity:
    - Renamed method parameters and return types to use consistent DTO classes.
    - Updated method names to align with the new DTO classes.
    - Improved readability and maintained consistency in the interface.
    
    These changes ensure consistency in the ImageService interface for better code organization and maintainability.

--- src/main/java/com/mpm/beforeandafter/image/service/ImageService.java
+++ src/main/java/com/mpm/beforeandafter/image/service/ImageService.java
@@ -1,17 +1,15 @@
 package com.mpm.beforeandafter.image.service;
 
-import com.mpm.beforeandafter.image.dto.CreateImageRequestDTO;
-import com.mpm.beforeandafter.image.dto.GetAllImagesResponseDTO;
-import com.mpm.beforeandafter.image.dto.GetImagesRequestByStatusApprovalDTO;
+import com.mpm.beforeandafter.image.dto.*;
 import com.mpm.beforeandafter.image.model.Image;
 
 import java.util.List;
 
 public interface ImageService {
 
-    List<Image> getImagesByApprovalStatus(GetImagesRequestByStatusApprovalDTO request);
+    List<GetImagesResponseByStatusApprovalDTO> getImagesByApprovalStatus(GetImagesRequestByStatusApprovalDTO request);
 
-    Image createImage(CreateImageRequestDTO request);
+    CreateImageResponseDTO createImage(CreateImageRequestDTO request);
 
     List<Image> getImagesByCategoryAndCity(Long categoryId, String cityName);
 

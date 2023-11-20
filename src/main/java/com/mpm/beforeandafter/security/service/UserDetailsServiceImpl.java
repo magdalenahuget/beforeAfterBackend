d4da151f4fc33773edffd7949153790ec8e5ab07
magdalenahuget <magdalena.huget@gmail.com>
2023-11-20 15:36:15 +0100

    Feature: implement loadUserByUsername to create userDetails object

---
+++ src/main/java/com/mpm/beforeandafter/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.mpm.beforeandafter.security.service;
+
+import com.mpm.beforeandafter.user.model.User;
+import com.mpm.beforeandafter.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        System.out.println("Email = " + email);
+        User user = userRepository.findByEmail(email).orElseThrow(() ->
+                new UsernameNotFoundException("User Not Found with username: " + email));
+        System.out.println("User = " + user);
+        List<SimpleGrantedAuthority> roles = new ArrayList<>();
+        String roleName = user.getRole().getName();
+        roles.add(new SimpleGrantedAuthority(roleName));
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), roles);
+    }
+}

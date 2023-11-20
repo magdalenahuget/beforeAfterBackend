04f189968703ee4d48a48dcc57e0e57cb317e39a
magdalenahuget <magdalena.huget@gmail.com>
2023-11-20 15:34:34 +0100

    Feature: add findByEmail method

--- src/main/java/com/mpm/beforeandafter/user/repository/UserRepository.java
+++ src/main/java/com/mpm/beforeandafter/user/repository/UserRepository.java
@@ -12,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByRoleName(String roleName);
 
     Optional<User> findByName(String username);
+
+    Optional<User> findByEmail(String email);
 }

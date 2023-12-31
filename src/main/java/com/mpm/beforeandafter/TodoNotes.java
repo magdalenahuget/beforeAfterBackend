3ffe108190a30d45718c2b98c363f07f52ff62a2
magdalenahuget <magdalena.huget@gmail.com>
2023-12-31 13:15:07 +0100

    Add class just for todo tasks

---
+++ src/main/java/com/mpm/beforeandafter/TodoNotes.java
@@ -0,0 +1,11 @@
+package com.mpm.beforeandafter;
+
+public class TodoNotes {
+    // TODO:
+    //  - Update README.md
+    //  - Add same loggers to all operations
+    //  - Handle better exceptions
+    //  - Add interfaces for mappers
+    //  - Add more tests
+    //
+}
